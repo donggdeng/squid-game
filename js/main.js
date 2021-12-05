55aca0959a30afe52886d5a8f5c0d059af7c29a4
Deng <donggdeng@gmail.com>
2021-12-05 16:40:47 +0800

    create a doll and moke it rotatable

---
+++ js/main.js
@@ -0,0 +1,69 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+renderer.setClearColor(0xb7c3f3, 1);
+
+const light = new THREE.AmbientLight( 0xffffff ); 
+scene.add( light );
+
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+camera.position.z = 5;
+
+const loader = new THREE.GLTFLoader();
+
+
+
+
+class Doll{
+    constructor(){
+        loader.load("../models/scene.gltf", (gltf) => {
+            scene.add(gltf.scene);
+            gltf.scene.scale.set(.4, .4, .4);
+            gltf.scene.position.set(0, -1, 0);
+            this.doll = gltf.scene;
+        })
+    }
+
+    lookBackward(){
+        gsap.to(this.doll.rotation, {y: -3.15, duration: .45})
+    }
+
+    lookForward(){
+        gsap.to(this.doll.rotation, {y: 0, duration: .45})
+    }
+ }
+
+//  function createTrack(){
+
+//  }
+
+
+let doll = new Doll();
+setTimeout(() => {
+    doll.lookBackward();
+}, 1000)
+
+function animate() {
+	requestAnimationFrame( animate );
+
+
+	renderer.render( scene, camera );
+}
+animate();
+
+window.addEventListener( 'resize', onWindowResize, false);
+
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
